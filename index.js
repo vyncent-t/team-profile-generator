f6dc134d5fbe00c3c480a96a964f04d4c8e53b3b
Vincent T <vyncode@gmail.com>
2021-05-14 20:31:47 -0400

    fixed finalizer logic

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const functions = {
 }
 module.exports = functions;
 
+const fs = require("fs")
 const inquirer = require("inquirer");
 const BottomBar = require("inquirer/lib/ui/bottom-bar");
 
@@ -133,14 +134,8 @@ function teamBuild() {
                 teamFinalizer()
                 console.log("boot team finisher")
                 // FILE CREATE THE HTML
-
-
-
-
-                //MAY NEED TO DROP ENTIRE HTML VAR HERE
             }
         })
-
 }
 
 function addEngineer() {
@@ -252,26 +247,59 @@ function teamFinalizer() {
         </div>`
             console.log(`updated page text for: ${workTeam[i].name}`)
         }
-        // // push string prop to new array
-        // for (i = 0; i < workTeam.length; i++) {
-        //     // need new array
-        // }
-        let cardBase = `
-      <div class="card" style="width: 18rem;">
-        <div class="card-header"> 
-            <div> <p>${workTeam[i].name}</p> </div>
-            <div> <p>${workTeam[i].role}</p> </div>
-        </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${workTeam[i].id}</li>
-                <li class="list-group-item">Email: ${workTeam[i].email}</li>
-            </ul>
-        </div>
-    `
-        let teamPageContent = workTeam.map(text => text.pagetext)
-        let finalPageContent = teamPageContent.join(" ")
-        console.log(teamPageContent)
     }
+    // // push string prop to new array
+    // for (i = 0; i < workTeam.length; i++) {
+    //     // need new array
+    // }
+    //     let cardBase = `
+    //   <div class="card" style="width: 18rem;">
+    //     <div class="card-header"> 
+    //         <div> <p>${workTeam[i].name}</p> </div>
+    //         <div> <p>${workTeam[i].role}</p> </div>
+    //     </div>
+    //         <ul class="list-group list-group-flush">
+    //             <li class="list-group-item">ID: ${workTeam[i].id} </li>
+    //             <li class="list-group-item">Email: ${workTeam[i].email} </li>
+    //         </ul>
+    //     </div>`
 
+    let teamPageContent = workTeam.map(text => text.pagetext)
+
+    let finalPageContent = teamPageContent.join(" ")
+
+    console.log(teamPageContent)
+
+    let basePage = `
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Team builder</title>
+  </head>
+  <body class="container-fluid">
+      <div class=" min-hieghtd-block w-100 h-50 bg-primary">
+          <h1 class="text-center text-light">Hello world</h1>
+      </div>
+    <div class="d-flex ">
+        <div class="d-flex justify-content-around"> ${finalPageContent} </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
+</html>
+        `;
+
+    fs.writeFile("index.html", basePage, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
 }
 
